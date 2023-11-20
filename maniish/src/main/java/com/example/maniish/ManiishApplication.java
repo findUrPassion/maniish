bd92244b15228088f3544c95b263d4db7abd6b21
Manish Kumar <maniishkr87@gmail.com>
2023-11-20 08:00:04 -0500

    pushing data to github

---
+++ maniish/src/main/java/com/example/maniish/ManiishApplication.java
@@ -0,0 +1,14 @@
+package com.example.maniish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManiishApplication.class, args);
+		System.out.println("First springboot application");
+	}
+
+}
